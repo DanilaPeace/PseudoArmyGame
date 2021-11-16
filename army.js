9796a4221f7ba277a4988244ba5b637e8f732b82
Danila <peace_are_off@bk.ru>
2021-11-16 14:55:12 +0300

    The first working version of the method
    There is one promlem when we use big and small numbers like [1, 300, 1]

--- army.js
+++ army.js
@@ -1,3 +1,30 @@
-function armyGetResorces(resource) {
-
-}
+function armyTakeResorces(resources, carrying) {
+    let result = [];
+  
+    // Get the amount of all resouces it is 100%
+    let amountAllResouces = resources.reduce(
+      (previousValue, currentValue) => previousValue + currentValue
+    );
+  
+    // Part of the resources of this type from the total amount of resources
+    let partOfResouses;
+    // Resources that take the army
+    let takingResouse;
+  
+    // Filling the resulting array
+    for (const currentResorceAmount of resources) {
+      partOfResouses = currentResorceAmount / amountAllResouces;
+  
+      takingResouse = Math.round(carrying * partOfResouses);
+  
+      if (takingResouse > currentResorceAmount) {
+        // Army will take all the resources of this type
+        // if the village doesn't have enough resources of this type
+        result.push(currentResorceAmount);
+      } else {
+        result.push(takingResouse);
+      }
+    }
+  
+    return result;
+  }
