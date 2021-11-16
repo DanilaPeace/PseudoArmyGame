c036b0d7e817c05e1f4b4ef53e93fb0053acf3d5
Danila <peace_are_off@bk.ru>
2021-11-16 20:43:45 +0300

    Added a value that keeps track of already harvested resources

--- army.js
+++ army.js
@@ -1,30 +1,50 @@
 function armyTakeResorces(resources, carrying) {
-    let result = [];
-  
-    // Get the amount of all resouces it is 100%
-    let amountAllResouces = resources.reduce(
-      (previousValue, currentValue) => previousValue + currentValue
-    );
-  
-    // Part of the resources of this type from the total amount of resources
-    let partOfResouses;
-    // Resources that take the army
-    let takingResouse;
-  
-    // Filling the resulting array
-    for (const currentResorceAmount of resources) {
-      partOfResouses = currentResorceAmount / amountAllResouces;
-  
-      takingResouse = Math.round(carrying * partOfResouses);
-  
-      if (takingResouse > currentResorceAmount) {
-        // Army will take all the resources of this type
-        // if the village doesn't have enough resources of this type
-        result.push(currentResorceAmount);
-      } else {
-        result.push(takingResouse);
-      }
+  let result = [];
+
+  // Get the amount of all resouces
+  let amountAllResources = resources.reduce(
+    (previousValue, currentValue) => previousValue + currentValue
+  );
+
+  // Part of the resources of this type from the total amount of resources
+  let partOfResources;
+  // Resources that take the army
+  let takingResources;
+  // Resources that the army has already taken
+  let takedRes = 0;
+
+  // Filling the resulting array
+  for (let index = 0; index < resources.length; index++) {
+    let currentResorceAmount = resources[index];
+    partOfResources = currentResorceAmount / amountAllResources;
+    takingResources = Math.round(carrying * partOfResources);
+
+    if (takingResources > currentResorceAmount) {
+      // Army will take all the resources of this type
+      // Because the village doesn't have resources equal to'takingResources'
+      result.push(currentResorceAmount);
+      takedRes += currentResorceAmount;
+    } else if (
+        index === resources.length - 1) {
+      result.push(carrying - takedRes);
+    } else {
+      result.push(takingResources);
+      takedRes += takingResources;
     }
-  
-    return result;
   }
+
+  return result;
+}
+
+let error = 0;
+for (let i = 0; i < 500; i++) {
+  let res = armyTakeResorces([1, 200, 102, 1, 3, 1], i);
+  let controlSumm = res.reduce(
+    (previousValue, currentValue) => previousValue + currentValue
+  );
+  if (i === controlSumm) {
+    error++;
+  }
+  console.log(`${i === controlSumm} ${i} == ${controlSumm} ${res}`);
+}
+console.log("ERROR:", error);
