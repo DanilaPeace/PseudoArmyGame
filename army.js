8ae5fd7145573e95a47986dd7d00fcd111568229
Danila <peace_are_off@bk.ru>
2021-11-17 08:57:15 +0300

    The version of task with a check resulting array

--- army.js
+++ army.js
@@ -3,38 +3,58 @@ function armyTakeResorces(resources, carrying) {
 
   // Get the amount of all resources
   let amountAllResources = resources.reduce(
-    (previousValue, currentValue) => previousValue + currentValue
+      (previousValue, currentValue) => previousValue + currentValue
   );
 
   // Part of the resources of this type from the total amount of resources
   let partOfResources;
   // Resources that take the army
   let takingResources;
-  // Resources that the army has already taken
-  let takedRes = 0;
+  // The total amount of resources that the army took
+  let controlSummOfResources = 0;
+
+  // If there are not enough resources in the village 
+  if (amountAllResources < carrying) return resources;
 
   // Filling the resulting array
   for (let index = 0; index < resources.length; index++) {
-    let currentResorceAmount = resources[index];
-    partOfResources = currentResorceAmount / amountAllResources;
-    takingResources = Math.round(carrying * partOfResources);
+      let currentResourceAmount = resources[index];
 
-    if (takingResources > currentResorceAmount) {
-      // Army will take all the resources of this type
-      // Because the village doesn't have resources equal to'takingResources'
-      result.push(currentResorceAmount);
-      takedRes += currentResorceAmount;
-    
-    } else if (
-      index === resources.length - 1 &&
-      0 < (carrying - takedRes) &&
-      (carrying - takedRes) <= currentResorceAmount) {
+      partOfResources = currentResourceAmount / amountAllResources;
+      takingResources = Math.round(carrying * partOfResources);
+      let freePlace = carrying - controlSummOfResources;
 
-      result.push(carrying - takedRes);
-    } else {
-      result.push(takingResources);
-      takedRes += takingResources;
-    }
+      // To avoid overflow at the end
+      if (takingResources > freePlace && takingResources <= currentResourceAmount) {
+          result.push(freePlace);
+          controlSummOfResources += freePlace;
+          resources[index] -= freePlace;
+      } else {
+          result.push(takingResources);
+          controlSummOfResources += takingResources;
+          resources[index] -= takingResources;
+      }
+  }
+  
+  //
+  // Check the result
+  // 
+
+  // This is a check if the army still has place for resources from the village
+  let i = 0;
+  while (controlSummOfResources < carrying) {
+
+      if (resources[i]) {
+          // To distribute resources evenly
+          result[i] += 1;
+          controlSummOfResources +=1;
+          resources[i] -= takingResources;
+      }
+
+      i++;
+      if (i === resources.length) {
+          i = 0;
+      }
   }
 
   return result;
